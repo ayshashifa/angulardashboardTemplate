03ff148064ffcadc573bd066546e357778d4d32e
Ayshashifa <vaseemaysha@gmail.com>
2024-01-26 23:30:46 +0530

    src file

---
+++ src/app/constants/app.constants.ts
@@ -0,0 +1,18 @@
+
+export const AppConstants = {
+  SERVER_CONFIG: {
+    // DEVELOPMENT: 'https://tnchampions.proz.in',
+    // LIVE: 'https://tnchampions1.sdat.in',
+    LIVE:'http://localhost:8000'
+  },
+};
+export function backendUrl() {
+  return AppConstants.SERVER_CONFIG.LIVE   
+}
+function createUrl(urlName: string) {
+  return backendUrl() + urlName
+}
+
+export const appApiResources = {
+   getexpenditurelist:createUrl('/aysha/tabledetails')
+};
